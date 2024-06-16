353b2d215ce0ea5ce7d9413b676d411532caf316
arhan321 <arhanmcz@gmail.com>
2024-06-16 19:07:49 +0700

    fresh from oven

--- crud_by_arhan/crud_by_djn/routes/web.php
+++ crud_by_arhan/crud_by_djn/routes/web.php
@@ -7,15 +7,19 @@ use App\Http\Controllers\GuruController;
 //     return view('welcome');
 // });
 
-Route::get('/',[GuruController::class,'index'])->name('guru.index');
+//route di definisikan satu persatu
+// Route::get('/',[GuruController::class,'index'])->name('guru.index');
 
-Route::get('/create',[GuruController::class, 'create'])->name('guru.create');
-Route::post('/create',[GuruController::class, 'store'])->name('guru.store');
+// Route::get('/create',[GuruController::class, 'create'])->name('guru.create');
+// Route::post('/create',[GuruController::class, 'store'])->name('guru.store');
 
-Route::get('/show/{id}',[GuruController::class, 'show'])->name('guru.show');
+// Route::get('/show/{id}',[GuruController::class, 'show'])->name('guru.show');
 
-Route::get('/edit/{id}', [GuruController::class, 'edit'])->name('guru.edit');
-Route::put('/edit/{id}', [GuruController::class, 'update'])->name('guru.update');
+// Route::get('/edit/{id}', [GuruController::class, 'edit'])->name('guru.edit');
+// Route::put('/edit/{id}', [GuruController::class, 'update'])->name('guru.update');
 
+// Route::delete('/delete/{id}',[GuruController::class, 'destroy'])->name('guru.destroy');
 
-Route::delete('/delete/{id}',[GuruController::class, 'destroy'])->name('guru.destroy');
+//atau pake yang ini sudah mendefinisikan semua route
+Route::get('/', [GuruController::class, 'index']);
+Route::resource('guru', GuruController::class);
