74b06baf14c46293b695647a0986199973901580
arhan321 <arhanmcz@gmail.com>
2024-06-19 10:06:28 +0700

    fresh from oven

--- api_laravel_pert9/api/app/Providers/AuthServiceProvider.php
+++ api_laravel_pert9/api/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -31,8 +32,10 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if ($request->input('password')) {
+                return DB::connection('api_token')->table('users')
+                    ->where('password', $request->input('password'))
+                    ->first();
             }
         });
     }
